211e51ff16cff720c28ee4d4d0748ec0c5dca12b
Galo Coloma <galo.coloma@gmail.com>
2025-11-15 19:19:51 -0500

    Update line ending
    update line ending to LF

--- problems/single_number/single-number.go
+++ problems/single_number/single-number.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-/* 
+/*
 
 136. Single Number
 
@@ -10,7 +10,7 @@ Given a non-empty array of integers nums, every element appears twice except for
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 
- 
+
 
 Example 1:
 
@@ -30,14 +30,13 @@ Input: nums = [1]
 
 Output: 1
 
- 
+
 
 Constraints:
 
 1 <= nums.length <= 3 * 104
 -3 * 104 <= nums[i] <= 3 * 104
 Each element in the array appears twice except for one element which appears only once. */
-
 
 func singleNumber(nums []int) int {
 	result := 0
